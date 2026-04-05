e13c6d16f72126bad739bcb748b7017556691f3c
lyy351 <1806921127@qq.com>
2026-04-05 19:38:49 +0800

    Update data.js

--- data.js
+++ data.js
@@ -39,7 +39,7 @@ window.SITE_DATA = {
   minorCourses: [
     {
       title: "漫画达人技法圣经 透视基础篇",
-      author: "HARD DELUXE (Z-Library)",
+      author: "HARD DELUXE",
       link: "https://www.kdocs.cn/l/cmcugKzsp4oJ",
       cover: "https://img9.doubanio.com/view/subject/l/public/s26684924.jpg",
       type: "书",
@@ -47,7 +47,7 @@ window.SITE_DATA = {
     },
     {
       title: "达·芬奇笔记",
-      author: "美国纽约大都 (Z-Library)",
+      author: "美国纽约大都",
       link: "https://www.kdocs.cn/l/ctu57DG5oRGL",
       cover: "https://img1.doubanio.com/view/subject/l/public/s10024950.jpg",
       type: "书",
@@ -55,7 +55,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法 动漫素材篇",
-      author: "C·C动漫社编著 (Z-Library)",
+      author: "C·C动漫社编著",
       link: "https://www.kdocs.cn/l/cdVELxHyPmzF",
       cover: "https://img1.doubanio.com/view/subject/l/public/s8500100.jpg",
       type: "书",
@@ -63,7 +63,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法 运动篇",
-      author: "立中顺平 (Z-Library)",
+      author: "立中顺平",
       link: "https://www.kdocs.cn/l/cpwxTUX7aD1b",
       cover: "https://img1.doubanio.com/view/subject/l/public/s3431648.jpg",
       type: "书",
@@ -71,7 +71,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法 质感表现篇",
-      author: "瓦屋根, 竜田, 林晃 (Z-Library)",
+      author: "瓦屋根, 竜田, 林晃",
       link: "https://www.kdocs.cn/l/cdl0MBOK6M1D",
       cover: "https://img1.doubanio.com/view/subject/l/public/s7025978.jpg",
       type: "书",
@@ -79,7 +79,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法-人物结构篇",
-      author: "荻野仁主编 (Z-Library)",
+      author: "荻野仁主编",
       link: "https://www.kdocs.cn/l/cgZ29BEh7Z8Z",
       cover: "https://img1.doubanio.com/view/subject/l/public/s6477308.jpg",
       type: "书",
@@ -87,7 +87,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法头身比篇",
-      author: "李景岩 (Z-Library)",
+      author: "李景岩",
       link: "https://www.kdocs.cn/l/cptUIJl406yW",
       cover: "https://img9.doubanio.com/view/subject/l/public/s2355395.jpg",
       type: "书",
@@ -95,7 +95,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法-草图篇",
-      author: "林晃 (Z-Library)",
+      author: "林晃",
       link: "https://www.kdocs.cn/l/cveprdm5ZbWu",
       cover: "https://img9.doubanio.com/view/subject/l/public/s5762745.jpg",
       type: "书",
@@ -103,7 +103,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法—透视篇",
-      author: "林晃, 松本刚彦, 森田和明 (Z-Library)",
+      author: "林晃, 松本刚彦, 森田和明",
       link: "https://www.kdocs.cn/l/chnG7eZv2rYI",
       cover: "https://img3.doubanio.com/view/subject/l/public/s11186037.jpg",
       type: "书",
@@ -111,7 +111,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法—基础篇",
-      author: "林晃, 松本刚彦, 森田和明 (Z-Library)",
+      author: "林晃, 松本刚彦, 森田和明",
       link: "https://www.kdocs.cn/l/cbIztlmf7OYf",
       cover: "https://img3.doubanio.com/view/subject/l/public/s2676523.jpg",
       type: "书",
@@ -119,7 +119,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法完全自学教程",
-      author: "超级漫画素描技法完全自学 (Z-Library)",
+      author: "超级漫画素描技法完全自学",
       link: "https://www.kdocs.cn/l/cr7TaAcfbsni",
       cover: "https://s41.ax1x.com/2026/04/05/petzKlF.jpg",
       type: "书",
@@ -127,7 +127,7 @@ window.SITE_DATA = {
     },
     {
       title: "韓國繪師的角色繪製重點攻略Vol.2",
-      author: "崔元喜 (Z-Library)",
+      author: "崔元喜",
       link: "https://www.kdocs.cn/l/csxJne945sIE",
       cover: "https://img3.doubanio.com/view/subject/l/public/s34367867.jpg",
       type: "书",
@@ -135,7 +135,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法 用照片学漫画篇",
-      author: "C·C动漫社编著 (Z-Library)",
+      author: "C·C动漫社编著",
       link: "https://www.kdocs.cn/l/cvGwduaiTqpM",
       cover: "https://s41.ax1x.com/2026/04/05/petzqpT.png",
       type: "书",
@@ -143,7 +143,7 @@ window.SITE_DATA = {
     },
     {
       title: "漫画技法圣经",
-      author: "C.C动漫社 (Z-Library)",
+      author: "C.C动漫社",
       link: "https://www.kdocs.cn/l/cf47F1069pO4",
       cover: "https://img1.doubanio.com/view/subject/l/public/s8977360.jpg",
       type: "书",
@@ -151,7 +151,7 @@ window.SITE_DATA = {
     },
     {
       title: "透视画法入门",
-      author: "约瑟夫·德·阿梅利奥 (Z-Library)",
+      author: "约瑟夫·德·阿梅利奥",
       link: "https://www.kdocs.cn/l/ca7niF7I43L2",
       cover: "https://img9.doubanio.com/view/subject/l/public/s32744224.jpg",
       type: "书",
@@ -159,7 +159,7 @@ window.SITE_DATA = {
     },
     {
       title: "动画大师课：场景透视",
-      author: "马科斯·马特乌-梅斯特 (Z-Library)",
+      author: "马科斯·马特乌-梅斯特",
       link: "https://www.kdocs.cn/l/cubxambCcrTz",
       cover: "https://img3.doubanio.com/view/subject/l/public/s34727187.jpg",
       type: "书",
@@ -167,7 +167,7 @@ window.SITE_DATA = {
     },
     {
       title: "艺用人体透视图集",
-      author: "约翰•科迪 郎•特里贝尔 (Z-Library)",
+      author: "约翰•科迪 郎•特里贝尔",
       link: "https://www.kdocs.cn/l/cviVsmp6XdrO",
       cover: "https://img1.doubanio.com/view/subject/l/public/s27168948.jpg",
       type: "书",
@@ -175,7 +175,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法 动漫素材篇 (重复链接)",
-      author: "C·C动漫社编著 (Z-Library)",
+      author: "C·C动漫社编著",
       link: "https://www.kdocs.cn/l/caH6L3z0HgRd",
       cover: "https://cdn.weread.qq.com/outpic/339/3002505339.jpg",
       type: "书",
@@ -183,7 +183,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法-草图篇 (另一链接)",
-      author: "林晃 (Z-Library)",
+      author: "林晃",
       link: "https://www.kdocs.cn/l/cekbwGZ7EdYZ",
       cover: "https://img9.doubanio.com/view/subject/l/public/s5762745.jpg",
       type: "书",
@@ -191,7 +191,7 @@ window.SITE_DATA = {
     },
     {
       title: "超级漫画素描技法—透视篇 (另一链接)",
-      author: "林晃, 松本刚彦, 森田和明 (Z-Library)",
+      author: "林晃, 松本刚彦, 森田和明",
       link: "https://www.kdocs.cn/l/ccA8oS2jBbkT",
       cover: "https://img3.doubanio.com/view/subject/l/public/s11186037.jpg",
       type: "书",
@@ -199,7 +199,7 @@ window.SITE_DATA = {
     },
     {
       title: "Anatomy of Facial Expression",
-      author: "Uldis Zarins (z-library.sk)",
+      author: "Uldis Zarins",
       link: "https://www.kdocs.cn/l/cks1xH3T3Ktr",
       cover: "https://img9.doubanio.com/view/subject/l/public/s33445295.jpg",
       type: "书",
@@ -207,7 +207,7 @@ window.SITE_DATA = {
     },
     {
       title: "制造漫画 (Making comics)",
-      author: "斯科特·麦克劳德 (z-library.sk)",
+      author: "斯科特·麦克劳德",
       link: "https://www.kdocs.cn/l/cnSkwAJUhVAy",
       cover: "https://img3.doubanio.com/view/subject/l/public/s6144382.jpg",
       type: "书",
@@ -223,7 +223,7 @@ window.SITE_DATA = {
     },
     {
       title: "谈绘画构图",
-      author: "高宗英著 (z-library.sk)",
+      author: "高宗英著",
       link: "https://www.kdocs.cn/l/ckR8ZOY7TQEG",
       cover: "https://img1.doubanio.com/view/subject/l/public/s35033338.jpg",
       type: "书",
@@ -231,7 +231,7 @@ window.SITE_DATA = {
     },
     {
       title: "摄影构图与用光零基础入门与提高",
-      author: "郑志强 (Z-Library)",
+      author: "郑志强",
       link: "https://www.kdocs.cn/l/ctPPJXfMT6qe",
       cover: "https://cdn.weread.qq.com/weread/cover/51/cpplatform_5ma8pso9m21pzubgxgjsgs/t6_cpplatform_5ma8pso9m21pzubgxgjsgs1682671978.jpg",
       type: "书",
@@ -239,7 +239,7 @@ window.SITE_DATA = {
     },
     {
       title: "色彩与光线：写实主义绘画指南",
-      author: "詹姆斯·格尔尼 (Z-Library)",
+      author: "詹姆斯·格尔尼",
       link: "https://www.kdocs.cn/l/cqyi5hlxvxQW",
       cover: "https://img1.doubanio.com/view/subject/l/public/s29498960.jpg",
       type: "书",
@@ -247,7 +247,7 @@ window.SITE_DATA = {
     },
     {
       title: "几何图案构成",
-      author: "李宗禹,王玉,李东 (z-library.sk)",
+      author: "李宗禹,王玉,李东",
       link: "https://www.kdocs.cn/l/cd8r16Rrid0z",
       cover: "https://img3.doubanio.com/view/subject/l/public/s35257577.jpg",
       type: "书",
@@ -255,7 +255,7 @@ window.SITE_DATA = {
     },
     {
       title: "宝石 全世界130多种宝石的彩色图鉴",
-      author: "卡利·霍尔, 哈里·泰勒 摄影 (z-library.sk)",
+      author: "卡利·霍尔, 哈里·泰勒 摄影",
       link: "https://www.kdocs.cn/l/cbbEqlpfCoOm",
       cover: "https://s41.ax1x.com/2026/04/05/peNSUNq.png",
       type: "书",
@@ -263,7 +263,7 @@ window.SITE_DATA = {
     },
     {
       title: "The Layout Book 版式设计",
-      author: "安布罗斯, Gavin Ambrose, 哈里斯 (z-library.sk)",
+      author: "安布罗斯, Gavin Ambrose, 哈里斯",
       link: "https://www.kdocs.cn/l/chJjuVsOWpNi",
       cover: "https://img1.doubanio.com/view/subject/l/public/s4558060.jpg",
       type: "书",
@@ -271,7 +271,7 @@ window.SITE_DATA = {
     },
     {
       title: "How to render 产品渲染技法全教程",
-      author: "Bertling Thomas,Robertson Scott (z-library.sk)",
+      author: "Bertling Thomas,Robertson Scott",
       link: "https://www.kdocs.cn/l/cpm2B76bkHCm",
       cover: "https://img2.doubanio.com/view/subject/l/public/s34151541.jpg",
       type: "书",
@@ -279,7 +279,7 @@ window.SITE_DATA = {
     },
     {
       title: "How to Draw产品概念手绘教程",
-      author: "Scott Robertson,Thomas Bertling (z-library.sk)",
+      author: "Scott Robertson,Thomas Bertling",
       link: "https://www.kdocs.cn/l/cdYPjpuCxgrX",
       cover: "https://img9.doubanio.com/view/subject/l/public/s34008084.jpg",
       type: "书",
